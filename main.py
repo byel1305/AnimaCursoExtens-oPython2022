ae728b62da4b942fad8c5397bda6f2ce5d3c5b53
Gabryel Júnio Costa Teixeira <90285723+byel1305@users.noreply.github.com>
2022-10-20 00:54:07 +0000

    Aprendi a criar variáveis e mostra na tela.

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 # Meu primeiro projeto em Python!!
-#
 # print() = comando de saída
 
 print("ALO Mundo!")
+
+# Quando quiser guardar uma String!
+# (frase)
+nome = "Gabryel Junio"
+# Quando quiser guardar um número inteiro
+idade = 23
+
+
+
+# Exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
 
 
 
 """
 Gabryel Junio 
+
 """
