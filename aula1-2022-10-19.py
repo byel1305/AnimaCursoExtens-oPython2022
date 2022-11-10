8857f1743e5fcad43a3730dc62fc9559fce35e07
Gabryel Júnio Costa Teixeira <90285723+byel1305@users.noreply.github.com>
2022-11-10 00:14:58 +0000

    Exibir Lista

---
+++ aula1-2022-10-19.py
@@ -0,0 +1,31 @@
+# Meu primeiro projeto em Python!!
+# print() = comando de saída
+
+print("ALO Mundo!")
+
+# Quando quiser guardar uma String!
+# (frase)
+nome = "Gabryel Junio"
+# Quando quiser guardar um número inteiro
+idade = 23
+
+
+
+# Exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
+# Quando quiser exibir a frase"Minha idade é" completando com o conteúdo da variável idade
+#print("Meu nome é"+nome)
+print("Minha idade é "+ str(idade)+" anos")
+print(f"Minha idade é {idade} anos")
+print("minha idade é {} anos".format(idade))
+
+# Quando quiser exebir "Meu nome é ... e tenho ...
+#anos.." trocando pelas variáveis nome e idade
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
+
+
+"""
+Gabryel Junio 
+
+"""
