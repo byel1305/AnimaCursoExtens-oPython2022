439c46ddaef1469e72a178f50f6b9061c020c9bf
Gabryel Júnio Costa Teixeira <90285723+byel1305@users.noreply.github.com>
2022-11-23 21:38:24 +0000

    mostrei a função + lista + laço

--- aula3-2022-11-09b.py
+++ aula3-2022-11-09b.py
@@ -13,12 +13,24 @@ print(imposto2)
 #Criar uma função calcular_imposto() que calcular um imposto de 5% e retorna a quem pediu...
 #Isso é a declaração da função (Como ela funciona)
 def calcular_imposto(preço_produto):
-  imposto = preço_produto * 0.05
+  imposto = preço_produto * 0.07
   return imposto 
 
 #Aqui é o uso... aqui é imposto a calcular.. e exibir na tela
 preço = 299
 imposto = calcular_imposto(preço)
-print(imposto)
+print(f"Esse aqui é com a função (7%): {imposto}")
+
+#Explicação de variável local x global 
+print(imposto) #???
+preço_produto = 100
+print(preço_produto) #???
 
 
+#agora calcular imposto a alíquota agora é 7% 
+
+valores = [1.99, 24.50, 78.27, 1515.5]
+# se eu quiser calcular o imposto destes quatro valores... e exibir na tela assim: "O imposto de é ..."(10. preço, 20. imposto)
+for valor in valores:
+  print(f"0 imposto de {valor} é {calcular_imposto(valor)}")
+           
